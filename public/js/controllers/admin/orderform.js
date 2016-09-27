dc35679e6c88fae61a4e75eb6837d1d53605dd6e
Pelmen2la <pelmengi12>
2016-09-27 15:08:12 +0300

    reworked admin page css

--- public/js/controllers/admin/orderform.js
+++ public/js/controllers/admin/orderform.js
@@ -1,6 +1,6 @@
 angular.module('gsoapAdminApp.adminControllers')
     .controller('OrderFormController', ['$scope', '$state', '$stateParams', 'Order', function($scope, $state, $stateParams, Order) {
-        $scope.order = Order.get({id: $stateParams.id}, function() {
+        $scope.order = Order.get({id: $stateParams.id}, function(data) {
         });
         $scope.submitOrder = function() {
             $scope.order.$update({id: $scope.order._id}, function() {
