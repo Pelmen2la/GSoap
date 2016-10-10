16a9e45be98606de566810a993014e352f354164
Pelmen2la <pelmengi12>
2016-10-10 13:31:04 +0300

    Brands open card reworked into href

--- public/js/controllers/brandlist.js
+++ public/js/controllers/brandlist.js
@@ -1,8 +1,4 @@
 angular.module('gsoapApp.controllers').controller('BrandListController', ['$scope', '$state', 'Brand', function($scope, $state, Brand) {
     $scope.brands = Brand.query({}, function(data) {
     });
-
-    $scope.openBrandCard = function(name) {
-        $state.go('brandCard', {name: name});
-    };
 }]);
